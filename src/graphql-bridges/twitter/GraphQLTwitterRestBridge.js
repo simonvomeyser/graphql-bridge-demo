60f5c0e1b1748c1e7fee96ceddbe7dcf3e8aab3e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-11-21 19:20:42 +0100

    Setup project to develop demo and lib at the same time

--- src/graphql-bridges/twitter/GraphQLTwitterRestBridge.js
+++ src/graphql-bridges/twitter/GraphQLTwitterRestBridge.js
@@ -1,4 +1,4 @@
-import { GraphQlRestBridge } from 'graphql-bridge';
+import { GraphQlRestBridge } from '../../../graphql-bridge'; // @todo change to npm package later
 
 export default class GraphQLTwitterRestBridge extends GraphQlRestBridge {
   constructor() {
