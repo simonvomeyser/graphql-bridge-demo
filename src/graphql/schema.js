3f555a119236193d610ffa6bef79bf53a53089b7
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-11-21 18:06:23 +0100

    Prepare schema,  create user type and twitter mockup

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,13 +1,35 @@
-import { GraphQLString, GraphQLSchema, GraphQLObjectType } from 'graphql';
+import {
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLNonNull,
+} from 'graphql';
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    name: {
+      type: GraphQLString,
+    },
+    twitterIntegration: {
+      type: GraphQLString,
+      resolve() {
+        // @todo return Twitter Integration here
+        return '';
+      },
+    },
+  },
+});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
       User: {
-        type: GraphQLString,
-        resolve() {
-          return 'User';
+        type: UserType,
+        args: { name: { type: new GraphQLNonNull(GraphQLString) } },
+        resolve(parentValue, args) {
+          return { name: args.name };
         },
       },
     },
