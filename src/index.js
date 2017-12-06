5e923c298d62a230a9c3cbbc24c73a0650ea1d2c
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-12-06 14:02:49 +0100

    Import packages and make starting server async

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 import * as bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-
 import express from 'express';
+
+import {
+  makeRemoteExecutableSchema,
+  mergeSchemas,
+  introspectSchema,
+} from 'graphql-tools';
+import { createApolloFetch } from 'apollo-fetch';
+
 import schema from './graphql/schema';
 
 require('dotenv').config();
 
-const app = express();
+async function run() {
+  const app = express();
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+  app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-app.get('*', (req, res) => {
-  res.send('Visit /graphql to use this app');
-});
+  app.get('*', (req, res) => {
+    res.send('Visit /graphql to use this app');
+  });
 
-const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
-});
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
+  });
+}
+
+try {
+  run();
+} catch (e) {
+  console.log(e, e.message, e.stack);
+}
